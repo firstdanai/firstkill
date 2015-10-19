1bb4949dedce769e0d505cabb0826f2d091b91bf
First <danai.de@baac.or.th>
2015-10-19 13:17:00 +0700

    firstblood

---
+++ app/src/main/java/com/first/baac/baacrestaurant/MyOpenHelper.java
@@ -0,0 +1,16 @@
+package com.first.baac.baacrestaurant;
+
+import android.content.Context;
+
+/**
+ * Created by BAAC on 19/10/2558.
+ */
+public class MyOpenHelper {
+
+    //Explicit
+    private static  final String DATABASE_NAME="BAAC.db";
+    private static final int DATABASE_VERSION = 1;
+    public MyOpenHelper(Context context) {
+
+    }
+}
